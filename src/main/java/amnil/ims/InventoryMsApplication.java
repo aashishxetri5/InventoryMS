5b6a26944328eef615e0503de7ca9e5314d538d6
Aashish Katwal <aashiskutwal5@gmail.com>
2024-11-05 17:41:05 +0545

    initial commit

--- src/main/java/amnil/ims/InventoryMsApplication.java
+++ src/main/java/amnil/ims/InventoryMsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InventoryMsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InventoryMsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryMsApplication.class, args);
+    }
 
 }
