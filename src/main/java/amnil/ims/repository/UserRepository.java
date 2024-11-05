4bc2dad206fad3a9525a1473eb89c3fa345b02b7
Aashish Katwal <aashiskutwal5@gmail.com>
2024-11-05 17:41:50 +0545

    Signin and Signup implemented

--- src/main/java/amnil/ims/repository/UserRepository.java
+++ src/main/java/amnil/ims/repository/UserRepository.java
@@ -1,4 +1,15 @@
 package amnil.ims.repository;
 
-public interface UserRepository {
+import amnil.ims.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
