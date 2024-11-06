21b4f58a8d265503f0b3ccb22e623a6958e5c3ce
Aashish Katwal <aashiskutwal5@gmail.com>
2024-11-06 23:52:30 +0545

    Supplier CRUD and entity modification

--- src/main/java/amnil/ims/repository/SupplierRepository.java
+++ src/main/java/amnil/ims/repository/SupplierRepository.java
@@ -1,4 +1,9 @@
 package amnil.ims.repository;
 
-public interface SupplierRepository {
+import amnil.ims.model.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
 }
